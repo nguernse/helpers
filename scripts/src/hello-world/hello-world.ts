918d5f705916d79250528821b90b8e2ed2f595a3
ng <n.guerns@gmail.com>
2023-05-23 08:44:53 -0500

    chore: update hello world

--- scripts/src/hello-world/hello-world.ts
+++ scripts/src/hello-world/hello-world.ts
@@ -1,5 +1,3 @@
-const world = "world";
-
-export function hello(who: string = world): string {
-  return `Hello ${who}!`;
+export function hello(msg: string = "world"): string {
+  return `Hello ${msg}!`;
 }
