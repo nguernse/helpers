15399a4a2f3af4d4541859de96b4d5a58133287b
ng <n.guerns@gmail.com>
2023-06-09 15:10:58 -0500

    chore(scripts): added get-pagination

---
+++ scripts/src/pagination/get-pagination.test.ts
@@ -0,0 +1,83 @@
+import { getPagination, Page, Offset, IPagination } from "./get-pagination";
+
+interface TestDataInterface {
+  input: [Page, Offset];
+  output: IPagination;
+}
+
+const testData: TestDataInterface[] = [
+  {
+    input: [0, 10],
+    output: {
+      from: 0,
+      to: 9,
+      offset: 10,
+    },
+  },
+  {
+    input: [1, 10],
+    output: {
+      from: 10,
+      to: 19,
+      offset: 10,
+    },
+  },
+  {
+    input: [2, 10],
+    output: {
+      from: 20,
+      to: 29,
+      offset: 10,
+    },
+  },
+  {
+    input: [0, 5],
+    output: {
+      from: 0,
+      to: 4,
+      offset: 5,
+    },
+  },
+  {
+    input: [0, 25],
+    output: {
+      from: 0,
+      to: 24,
+      offset: 25,
+    },
+  },
+  {
+    input: [undefined, undefined],
+    output: {
+      from: 0,
+      to: 9,
+      offset: 10,
+    },
+  },
+  {
+    input: ["0", "10"],
+    output: {
+      from: 0,
+      to: 9,
+      offset: 10,
+    },
+  },
+  {
+    input: [0, undefined],
+    output: {
+      from: 0,
+      to: 9,
+      offset: 10,
+    },
+  },
+];
+
+describe("getPagination", () => {
+  testData.forEach(({ input, output }) => {
+    it(`getPagination(${input[0]}, ${input[1]}) --> {from: ${output.from}, to: ${output.to}, offest: ${output.offset}}`, () => {
+      const result = getPagination(...input);
+
+      expect(result).toMatchObject(output);
+    });
+  });
+});
---
+++ scripts/src/pagination/get-pagination.ts
@@ -0,0 +1,33 @@
+export type Page = string | number | undefined;
+export type Offset = string | number | undefined;
+
+export interface IPagination {
+  from: number;
+  to: number;
+  offset: number;
+}
+
+/**
+ * Generate pagination for a table, assuming starting row position is 0.
+ *
+ * @param page
+ * @param offset
+ * @returns IPagination
+ *
+ * @example
+ * getPagination(0, 10) => { from: 0, to: 9, offset: 10 }
+ * getPagination(1, 10) => { from: 10, to: 19, offset: 10 }
+ * getPagination(2, 10) => { from: 20, to: 29, offset: 10 }
+ */
+export const getPagination = (
+  page: Page = 0,
+  offset: Offset = 10
+): IPagination => {
+  page = typeof page === "undefined" ? 0 : +page;
+  offset = typeof offset === "undefined" ? 10 : +offset;
+
+  const from = page ? page * offset : 0;
+  const to = page ? from + offset - 1 : offset - 1;
+
+  return { from, to, offset };
+};
