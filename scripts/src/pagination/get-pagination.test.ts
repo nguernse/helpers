07f8d0fe2c4c9371c0318616c45b0a6260355ada
ng <n.guerns@gmail.com>
2023-06-09 15:24:58 -0500

    chore(scripts): update pagination testing

--- scripts/src/pagination/get-pagination.test.ts
+++ scripts/src/pagination/get-pagination.test.ts
@@ -1,11 +1,19 @@
 import { getPagination, Page, Offset, IPagination } from "./get-pagination";
 
 interface TestDataInterface {
-  input: [Page, Offset];
+  input: [Page?, Offset?];
   output: IPagination;
 }
 
 const testData: TestDataInterface[] = [
+  {
+    input: [],
+    output: {
+      from: 0,
+      to: 9,
+      offset: 10,
+    },
+  },
   {
     input: [0, 10],
     output: {
@@ -47,14 +55,6 @@ const testData: TestDataInterface[] = [
     },
   },
   {
-    input: [undefined, undefined],
-    output: {
-      from: 0,
-      to: 9,
-      offset: 10,
-    },
-  },
-  {
     input: ["0", "10"],
     output: {
       from: 0,
@@ -63,26 +63,24 @@ const testData: TestDataInterface[] = [
     },
   },
   {
-    input: [0, undefined],
+    input: [0],
     output: {
       from: 0,
       to: 9,
       offset: 10,
     },
   },
+  {
+    input: [undefined, 5],
+    output: {
+      from: 0,
+      to: 4,
+      offset: 5,
+    },
+  },
 ];
 
 describe("getPagination", () => {
-  it("getPagination() --> {from: 0, to: 9, offest: 10}", () => {
-    const result = getPagination();
-
-    expect(result).toMatchObject({
-      from: 0,
-      to: 9,
-      offset: 10,
-    });
-  });
-
   testData.forEach(({ input, output }) => {
     it(`getPagination(${input[0]}, ${input[1]}) --> {from: ${output.from}, to: ${output.to}, offest: ${output.offset}}`, () => {
       const result = getPagination(...input);
