00ab3b85c084db71b0307eb1911fff265b35ab32
ng <n.guerns@gmail.com>
2023-06-28 08:06:34 -0500

    chore(logger): updated logger factory

--- scripts/src/logger-factory/logger-factory.ts
+++ scripts/src/logger-factory/logger-factory.ts
@@ -1,13 +1,15 @@
 import { EnvironmentOptions, getEnv } from "../get-environment/get-environment";
 
 enum LoggerPrefixes {
-  INFO = "::info::",
-  DEBUG = "::debug::",
-  WARN = "::warn::",
-  ERROR = "::error::",
+  LOG = "log",
+  INFO = "info",
+  DEBUG = "debug",
+  WARN = "warn",
+  ERROR = "error",
 }
 
 interface ILogger {
+  log(message: string): void;
   info(message: string): void;
   debug(message: string): void;
   warn(message: string): void;
@@ -15,67 +17,84 @@ interface ILogger {
 }
 
 export class ProductionLogger implements ILogger {
-  info(message: string): void {}
+  log(message: string): void {}
+  info(message: string): void {
+    console.info(
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.INFO}]`,
+      message
+    );
+  }
   warn(message: string): void {
     console.warn(
-      `::${EnvironmentOptions.PRODUCTION}::`,
-      LoggerPrefixes.WARN,
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.WARN}]`,
       message
     );
   }
   debug(message: string): void {}
   error(message: string): void {
     console.error(
-      `${EnvironmentOptions.PRODUCTION}${LoggerPrefixes.ERROR}`,
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.ERROR}]`,
       message
     );
   }
 }
 
 export class StagingLogger implements ILogger {
-  info(message: string): void {}
+  log(message: string): void {}
+  info(message: string): void {
+    console.info(
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.INFO}]`,
+      message
+    );
+  }
   warn(message: string): void {
     console.warn(
-      `${EnvironmentOptions.STAGING}${LoggerPrefixes.WARN}`,
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.WARN}]`,
       message
     );
   }
   debug(message: string): void {
-    console.info(
-      `${EnvironmentOptions.STAGING}${LoggerPrefixes.INFO}`,
+    console.debug(
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.DEBUG}]`,
       message
     );
   }
   error(message: string): void {
     console.error(
-      `${EnvironmentOptions.STAGING}${LoggerPrefixes.ERROR}`,
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.ERROR}]`,
       message
     );
   }
 }
 
 export class DevelopmentLogger implements ILogger {
+  log(message: string): void {
+    console.log(
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.LOG}]`,
+      message
+    );
+  }
   info(message: string): void {
     console.info(
-      `${EnvironmentOptions.DEVELOPMENT}${LoggerPrefixes.INFO}`,
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.INFO}]`,
       message
     );
   }
   warn(message: string): void {
     console.warn(
-      `${EnvironmentOptions.DEVELOPMENT}${LoggerPrefixes.WARN}`,
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.WARN}]`,
       message
     );
   }
   debug(message: string): void {
     console.debug(
-      `${EnvironmentOptions.DEVELOPMENT}${LoggerPrefixes.DEBUG}`,
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.DEBUG}]`,
       message
     );
   }
   error(message: string): void {
     console.error(
-      `${EnvironmentOptions.DEVELOPMENT}${LoggerPrefixes.ERROR}`,
+      `[${EnvironmentOptions.PRODUCTION}::${LoggerPrefixes.ERROR}]`,
       message
     );
   }
