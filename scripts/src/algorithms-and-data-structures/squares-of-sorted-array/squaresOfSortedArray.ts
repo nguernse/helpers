a75c52a3bef672719cb3a441feeec2be53e913cf
ng <n.guerns@gmail.com>
2023-11-20 10:51:38 -0600

    chore(algo): comments for squares array

--- scripts/src/algorithms-and-data-structures/squares-of-sorted-array/squaresOfSortedArray.ts
+++ scripts/src/algorithms-and-data-structures/squares-of-sorted-array/squaresOfSortedArray.ts
@@ -3,6 +3,8 @@
  * return an array of the squares of each number sorted in non-decreasing order.
  *
  * @see https://leetcode.com/problems/squares-of-a-sorted-array/
+ *
+ * Both solutions are O(n)
  */
 export default function squaresOfSortedArray(nums: number[]): number[] {
   // Using built-in JavaScript methods
@@ -12,9 +14,14 @@ export default function squaresOfSortedArray(nums: number[]): number[] {
 
   // Using two-pointer technique
   const result: number[] = new Array(nums.length);
-  let insertIndex = nums.length - 1;
+
+  // the left and right pointer will be at each end of the nums array
+  // this will help traverse the array once, of O(n)
   let leftPointer = 0;
   let rightPointer = nums.length - 1;
+
+  // the insertIndex will keep track of where to insert the next square keeping sort order
+  let insertIndex = rightPointer;
 
   while (leftPointer <= rightPointer) {
     const a = Math.abs(nums[leftPointer]);
